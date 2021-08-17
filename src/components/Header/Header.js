08183ce781a55001e958391c4d721b24436cd3eb
leogtzr <leogutierrezramirez@gmail.com>
2021-08-17 16:06:01 -0600

    add select component and categories

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,43 @@
+import { TextField, createTheme, ThemeProvider, MenuItem } from '@material-ui/core';
 import React from 'react';
 import './Header.css';
+import categories from '../../data/categories';
 
 const Header = () => {
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#fff'
+            },
+            type: 'dark',
+        },
+    });
+
     return (
         <div className='header'>
             <span className='title'>
-                Holis....
+                WORD HUNT
             </span>
+            <div className='inputs'>
+                <ThemeProvider theme={darkTheme}>
+                    {/* <TextField id='standard-basic' label="Standard" /> */}
+                    <TextField
+                        id='standard-select-currency'
+                        select
+                        label="Select"
+                        helperText="Please select your currency"
+                    >
+                        {
+                            categories.map((category) => (
+                                <MenuItem >
+                                    {category.value}
+                                </MenuItem>
+                            ))
+                        }
+                    </TextField>
+                </ThemeProvider>
+            </div>
         </div>
     )
 }
